710e9aec5e1aee9e9dcd77217f31f980175f9a4a
Jamie Higgins <S00162685@mail.itsligo.ie>
2018-01-22 15:55:49 +0000

    Exercise 1.2, may need to update FeedbackRating

---
+++ WebApplication1/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public DateTime OrderedOn { get; set; }
+        public DateTime DeliveredOn { get; set; }
+        public double Value { get; set; }
+        [Range(0, 5)]
+        public int FeedbackRating { get; set; }
+    }
+}
